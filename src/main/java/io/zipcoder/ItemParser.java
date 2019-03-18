0165c80af4f903d9bbc11f8480368568a538f954
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-03-18 18:24:41 -0400

    all item parser tests pass

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -6,6 +6,9 @@ import io.zipcoder.utils.ItemParseException;
 import java.util.*;
 
 public class ItemParser {
+
+
+
     public List<Item> parseItemList(String valueToParse) {
 
         List<Item> parsedItemList = new ArrayList<>();
@@ -16,10 +19,9 @@ public class ItemParser {
             try {
                 parsedItemList.add(parseSingleItem(unparsedItem));
             } catch (ItemParseException e) {
-                e.printStackTrace();
+//                e.printStackTrace();
             }
         }
-
         return parsedItemList;
     }
 
@@ -27,46 +29,54 @@ public class ItemParser {
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
 
-        Map<String, String> kvpairs = itemMap(singleItem.replace("##", ""));
+        String cleanedupItem = singleItem.toLowerCase().replace("##", "");
+        Map<String, String> kvpairs = itemMap(cleanedupItem);
 
-        return new Item(kvpairs.get("name"),
+        return new Item(
+                kvpairs.get("name"),
                 Double.parseDouble(kvpairs.get("price")),
                 kvpairs.get("type"),
                 kvpairs.get("expiration"));
     }
 
 
-    //----------------------------
+
 
     //parse whole list into an array of item strings
     private String[] createItemArray(String wholeFile) {
         return parser(wholeFile, "##");
     }
 
-
-    //naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##
-    //parse an item string into a Map of fields
-    private Map<String, String> itemMap(String itemString) {
+    //parse an item string into a Map of fields for that item
+    private Map<String, String> itemMap(String itemString) throws ItemParseException {
         String[] fieldArray = createFieldInfoArray(itemString);
 
         Map<String, String> fieldMap = new HashMap<>();
+
         for (String field : fieldArray) {
-            String[] pair = fieldMap(field);
-            fieldMap.put(pair[0].toLowerCase(), pair[1].toLowerCase());
+            String[] pair = fieldsToMap(field);
+            try {
+                fieldMap.put(pair[0], pair[1]);
+            } catch(IndexOutOfBoundsException e) {
+                throw new ItemParseException();
+            }
         }
         return fieldMap;
     }
 
-    //naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##
-    //      to name:Milk price:3.23 type:Food expiration:1/25/2016
     //parse an item into an array of unparsed field strings
+    //      from naMe:Milk;price:3.23;type:Food;expiration:1/25/2016## to
+    //      [name:Milk, price:3.23, type:Food, expiration:1/25/2016]
+
     private String[] createFieldInfoArray(String itemArray) {
         return parser(itemArray, ";");
     }
-    //name:Milk
-    //      to [name, Milk]
+
+
     //parse unparsed field to create kv pair
-    private String[] fieldMap(String unparsedFieldInfo) {
+    //    from name:Milk -> [name, Milk]
+
+    private String[] fieldsToMap(String unparsedFieldInfo) {
         return parser(unparsedFieldInfo, "[:@^*%}]");
     }
 
